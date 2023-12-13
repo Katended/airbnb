bec51677dafb3de4f101131a9030af4ff23bdc6d
David Katende <katendedavidjoga@gmail.com>
2023-12-13 23:14:28 +0300

    Fix menu

--- app/javascript/controllers/model_controller.js
+++ app/javascript/controllers/model_controller.js
@@ -4,9 +4,9 @@ import { enter, leave, toggle } from 'el-transition';
 export default class extends Controller {
 
   connect() {
-    enter(document.getElementById('modal-wrapper'));
-    enter(document.getElementById('modal-backdrop'));
-    enter(document.getElementById('modal-panel'));
+    // enter(document.getElementById('modal-wrapper'));
+    // enter(document.getElementById('modal-backdrop'));
+    // enter(document.getElementById('modal-panel'));
 
     document.getElementById('modal-wrapper').addEventListener("click", this.closeModal);
 
@@ -20,6 +20,10 @@ export default class extends Controller {
       leave(document.getElementById('modal-backdrop'));
       leave(document.getElementById('modal-panel'));
     }
+  }
+
+  showModal() {
+
   }
 
 }
