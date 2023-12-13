06188986fcb86ced39b3732c57757e2b7259c185
David Katende <katendedavidjoga@gmail.com>
2023-12-13 22:25:40 +0300

    Add links to menu

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -3,9 +3,17 @@ import { enter, leave, toggle } from 'el-transition';
 
 export default class extends Controller {
 
-  static targets = ["openUserMenu"];
+  static targets = ["openUserMenu", "userAuthLink"];
   connect() {
     this.openUserMenuTarget.addEventListener("click", this.toggleDropdownMenu);
+    // this.signUpLink.addEventListener("click", this.toggleDropdownMenu);
+    // this.loginLink.addEventListener("click", this.toggleDropdownMenu);
+
+    this.userAuthLinkTargets.forEach((link) => {
+      link.addEventListener("click", () => {
+        console.log("clicked");
+      });
+    });
   }
 
   toggleDropdownMenu() {
