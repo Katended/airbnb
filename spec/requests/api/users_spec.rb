7e16bbabdf72a1a361b5e2bc4ae6b62aaf2748f5
David Katende <katendedavidjoga@gmail.com>
2024-01-05 19:08:38 +0300

    Add test 2

--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -1,19 +1,24 @@
 require 'rails_helper'
-RSpec.descibe "Api::Users", type: :request do
-    decribe 'GET Show' do
-
-        context "user exists" do
-            it "it is successfull" do
-                user = create(:user)
-                get api_user_path(user),header: { 'ACCEPT' => 'application/json' }
-                expect(response).to be_successful
-            end                      
-        end
-        context "user does not exist" do
-
-             it "it is not found" do
-                
-            end                      
-        end
+RSpec.describe "Api::Users", type: :request do
+  describe 'GET Show' do
+    context "user exists" do
+      it "is successful" do
+        user = create(:user)
+        get api_user_path(user), headers: { 'ACCEPT' => 'application/json' }
+        expect(response).to be_successful
+      end
     end
+  end
 end
+
+# RSpec.descibe "Api::Users", type: :request do
+#     decribe 'GET Show' do
+#         context "user exists" do
+#             it "it is successfull" do
+#                 user = create(:user)
+#                 get api_user_path(user),header: { 'ACCEPT' => 'application/json' }
+#                 expect(response).to be_successful
+#             end                      
+#         end
+#     end
+# end
