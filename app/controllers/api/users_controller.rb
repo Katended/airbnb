f552285e9ee17e5cf07264a9c022b1b89b784e29
David Katende <katendedavidjoga@gmail.com>
2024-01-08 12:53:04 +0300

    Add test

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,19 @@
 module Api
     class UsersController < ApplicationController
         def show
-            # user = user.find(params[:id])
-            # respond_to do |format|
-            #    render json: user.to_json, status: ok  
-            # end                      
+            user = User.find_by(id: params[:id])
+            respond_to do |format|
+               format.json {
+                    render json: user.to_json, status: :ok
+                }               
+            end
+            
+            rescue ActiveRecord::RecordNotFound => e
+                respond_to do |format|
+                    format.json {
+                        render json: {error: e.message }, status: :not_found
+                    }               
+                end                          
         end
-              
     end      
 end
