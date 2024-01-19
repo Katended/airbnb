2c84526d722e7ffeb5ea5c67240dd07cec3b4377
David Katende <katendedavidjoga@gmail.com>
2024-01-19 14:01:23 +0300

    Add axios

--- Gemfile
+++ Gemfile
@@ -52,7 +52,7 @@ gem "sassc-rails"
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
-
+gem 'axios_rails', '~> 0.7.0'
 
 group :development, :test do
  gem "factory_bot_rails"
--- app/javascript/controllers/users_by_email_auth_controller.js
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -1,17 +1,17 @@
 import { Controller } from "@hotwired/stimulus";
-// import axios from "axios";
+import axios from "axios";
+
 export default class extends Controller {
 
     static targets = ["email", "submit"];
 
     connect() {
-        // console.log('axios: ', axios);
+
         this.submitTarget.addEventListener('click', (e) => {
             e.preventDefault();
 
             if (this.emailTarget.value.length > 0) {
-                console.log("working");
-                // this.submitTarget.disabled = false;
+                console.log('axios: ', axios);
             } else {
                 //  this.submitTarget.disabled = true;
             }
