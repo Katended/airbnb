a6af4822e539bce65e60cbde9fb0bdf46b8be86d
David Katende <katendedavidjoga@gmail.com>
2024-01-24 16:30:30 +0300

    Add signup

--- app/javascript/controllers/users_by_email_auth_controller.js
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -1,18 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
-const axios = require('axios');
-
-
+import axios from "axios"
 export default class extends Controller {
     static targets = ["email", "submit"];
 
     connect() {
         this.submitTarget.addEventListener('click', (e) => {
             e.preventDefault();
-            alert('test');
+
             if (this.emailTarget.value.length > 0) {
                 axios.get('/api/users_by_email', {
-                    params: { email: this.emailTarget.value }
-                }).then(() => { Turbo.visit('/users/sin_in') }).catch((resposne) => { Turbo.visit('/users/sin_up') });
+                    params: { email: this.emailTarget.value },
+                    headers: { "accept": "application/json" }
+                }).then(() => { Turbo.visit('/users/sign_in') }).catch((resposne) => { Turbo.visit('/users/sign_up') });
             } else {
                 //  this.submitTarget.disabled = true;
             }
